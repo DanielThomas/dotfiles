e3dc864997d69be15687c198ac52c5104b14cbdd
Danny Thomas <Danny.Thomas@blackboard.com>
2014-03-17 13:24:12 +0000

    Make git aliases friendlier

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,3 +1,3 @@
-alias p4r="g p4r"
-alias p4s="g p4s"
+alias p4r="git p4 rebase"
+alias p4s="git p4 submit"
 alias gh="open \`git remote -v | grep git@github.com | grep fetch | head -1 | cut -f2 | cut -d' ' -f1 | sed -e's/:/\//' -e 's/git@/http:\/\//'\`"

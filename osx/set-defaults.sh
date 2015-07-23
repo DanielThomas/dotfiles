e453ff8d53f92528cfb8c3c87d55b66f1d67b524
Danny Thomas <dannyt@netflix.com>
2015-07-23 12:18:48 -0700

    Add window animation tweak

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -1,10 +1,14 @@
 ## Sensible defaults for Mac OS
 
-# Disable press-and-hold for keys in favor of key repeat.
+# Disable press-and-hold for keys in favor of key repeat
 defaults write -g ApplePressAndHoldEnabled -bool false
 
-# Show the ~/Library folder.
+# Show the ~/Library folder
 chflags nohidden ~/Library
 
-# Set a really fast key repeat.
+# Set a really fast key repeat
 defaults write NSGlobalDomain KeyRepeat -int 0
+
+# Showing and hiding sheets, resizing preference windows, zooming windows
+# float 0 doesn't work
+defaults write -g NSWindowResizeTime -float 0.001

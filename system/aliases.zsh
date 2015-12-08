acba8337594a50021fa898bb8e2afcd4b0eb55a1
Danny Thomas <dannyt@netflix.com>
2015-12-08 13:11:35 -0800

    Fix ls aliases so color=auto

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,8 +1,8 @@
 # overrides for ls
-alias ls="gls -F --color"
-alias l="ls -lAh --color"
-alias ll="ls -l --color"
-alias la="ls -A --color"
+alias ls="gls -F --color=auto"
+alias l="ls -lAh --color=auto"
+alias ll="ls -l --color=auto"
+alias la="ls -A --color=auto"
 
 # allow for common cd typo
 alias cd..="cd .."

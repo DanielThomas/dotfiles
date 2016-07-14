05161ad6e38f15d0ce887bfa5a46ead7b2dfd8bc
Danny Thomas <dannyt@netflix.com>
2016-07-14 12:19:33 -0700

    Add helper functions for Gradle options

---
+++ dev/functions.zsh
@@ -0,0 +1,22 @@
+function gradle_opts_unset() {
+    unset GRADLE_OPTS
+    gradle_enable_daemon
+}
+
+function gradle_opts_debug() {
+    export GRADLE_OPTS="-Xrunjdwp:server=y,transport=dt_socket,address=5005,suspend=y"
+    gradle_disable_daemon
+}
+
+function gradle_opts_profile() {
+    export GRADLE_OPTS="-agentpath:/Applications/YourKit.app/Contents/Resources/bin/mac/libyjpagent.jnilib=sampling,probe_disable=*,onexit=snapshot"
+    gradle_disable_daemon
+}
+
+function gradle_disable_daemon() {
+    ssed -i 's/org.gradle.daemon=true/org.gradle.daemon=false/g' ~/.gradle/gradle.properties
+}
+
+function gradle_enable_daemon() {
+    ssed -i 's/org.gradle.daemon=false/org.gradle.daemon=true/g' ~/.gradle/gradle.properties
+}

ba4f28aa65a05a7a8f9a3e6d3676a8c3a6b74c1e
Mike McGarr <mmcgarr@netflix.com>
2016-09-27 20:05:01 -0700

    Updates to my git aliases and added a jce config

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,3 +1,2 @@
 alias st="status -s"
-alias lg="log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)%Creset' --abbrev-commit --date=relative"
 alias gh="open \`git remote -v | grep git@github.com | grep fetch | head -1 | cut -f2 | cut -d' ' -f1 | sed -e's/:/\//' -e 's/git@/http:\/\//'\`"

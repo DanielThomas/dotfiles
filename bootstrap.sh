83afee90d59631e707130674ea0833ff8e4e99da
Danny Thomas <dannyt@netflix.com>
2016-10-10 14:55:40 -0700

    Fix run logic

--- bootstrap.sh
+++ bootstrap.sh
@@ -32,7 +32,7 @@ copy_file () {
 }
 
 open_file () {
-  open $1
+  run "open $1" "open $1"
   success "opened $1"
 }
 
@@ -144,7 +144,7 @@ install_file () {
 
 run_installers () {
   info 'running installers'
-  find . -name install.sh | while read installer ; do run "${installer}" ; done
+  find . -name install.sh | while read installer ; do run "${installer}" "${installer}" ; done
 
   info 'opening files'
   OLD_IFS=$IFS

d67ca5401772cfeeca8268db543d406f218725d7
Danny Thomas <dannyt@netflix.com>
2016-10-18 14:33:03 -0700

    GNU sed

--- dev/functions.zsh
+++ dev/functions.zsh
@@ -14,11 +14,11 @@ function gradle_opts_profile() {
 }
 
 function gradle_disable_daemon() {
-    sed -i 's/org.gradle.daemon=true/org.gradle.daemon=false/g' ~/.gradle/gradle.properties
+    gsed -i 's/org.gradle.daemon=true/org.gradle.daemon=false/g' ~/.gradle/gradle.properties
 }
 
 function gradle_enable_daemon() {
-    sed -i 's/org.gradle.daemon=false/org.gradle.daemon=true/g' ~/.gradle/gradle.properties
+    gsed -i 's/org.gradle.daemon=false/org.gradle.daemon=true/g' ~/.gradle/gradle.properties
 }
 
 function gradle_user_home_local() {

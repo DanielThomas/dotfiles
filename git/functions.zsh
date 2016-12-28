1ed02ef7d147d1bd15e0f00625aed997ad24770f
Danny Thomas <dannyt@netflix.com>
2016-12-28 14:08:28 -0800

    Add simple utility for BitBucket operations

--- git/functions.zsh
+++ git/functions.zsh
@@ -1,11 +1,2 @@
 # gitignore.io - http://www.gitignore.io/cli
 function gi() { curl http://www.gitignore.io/api/$@ ;}
-
-function bb() {
-    FETCH_URL=$(git remote -v | grep git@ | grep fetch | head -1 | cut -f2 | cut -d ' ' -f 1 | sed 's#ssh://git@##')
-    FETCH_HOST=$(echo "$FETCH_URL" | grep -o '[^:]*')
-    FETCH_PATH=$(echo "$FETCH_URL" | grep -o '/.*')
-    PROJECT=$(echo "$FETCH_PATH" | cut -d '/' -f 2)
-    REPO=$(echo "$FETCH_PATH" | cut -d '/' -f 3 | sed 's/\.git//')
-    open "https://$FETCH_HOST/projects/$PROJECT/repos/$REPO/browse"
-}

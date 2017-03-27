b0978ba1a9c079c98fb063b862bb11d72fef760c
Danny Thomas <dannyt@netflix.com>
2017-03-27 12:49:53 -0700

    Switch to camel case for Gradle functions to improve completion

--- jvm/functions.zsh
+++ jvm/functions.zsh
@@ -1,45 +1,45 @@
-function gradle_opts_unset() {
+function gradleOptsUnset() {
     unset GRADLE_OPTS
     gradle_enable_daemon
 }
 
-function gradle_opts_debug() {
+function gradleOptsDebug() {
     export GRADLE_OPTS="-Xrunjdwp:server=y,transport=dt_socket,address=5005,suspend=y"
     gradle_disable_daemon
 }
 
-function gradle_opts_yk() {
+function gradleOptsYk() {
     export GRADLE_OPTS="-agentpath:/Applications/YourKit.app/Contents/Resources/bin/mac/libyjpagent.jnilib"
     gradle_disable_daemon
 }
 
-function gradle_opts_yksample() {
+function gradleOptsYkSample() {
     export GRADLE_OPTS="-agentpath:/Applications/YourKit.app/Contents/Resources/bin/mac/libyjpagent.jnilib=sampling,probe_disable=*,onexit=snapshot"
     gradle_disable_daemon
 }
 
-function gradle_opts_ykalloc() {
+function gradleOptsYkAlloc() {
     export GRADLE_OPTS="-agentpath:/Applications/YourKit.app/Contents/Resources/bin/mac/libyjpagent.jnilib=alloceach=10,allocsizelimit=4096,onexit=snapshot"
     gradle_disable_daemon
 }
 
-function gradle_opts_ykalloc_count() {
+function gradleOptsYkAllocCount() {
     export GRADLE_OPTS="-agentpath:/Applications/YourKit.app/Contents/Resources/bin/mac/libyjpagent.jnilib=alloc_object_counting,onexit=snapshot"
     gradle_disable_daemon
 }
 
-function gradle_disable_daemon() {
+function gradleDisableDaemon() {
     gsed -i 's/org.gradle.daemon=true/org.gradle.daemon=false/g' ~/.gradle/gradle.properties
 }
 
-function gradle_enable_daemon() {
+function gradleEnableDaemon() {
     gsed -i 's/org.gradle.daemon=false/org.gradle.daemon=true/g' ~/.gradle/gradle.properties
 }
 
-function gradle_user_home_local() {
+function gradleUserHomeLocal() {
     export GRADLE_USER_HOME="$(pwd)/.gradle"
 }
 
-function gradle_user_home_unset() {
+function gradleUserHomeUnset() {
     unset GRADLE_USER_HOME
 }

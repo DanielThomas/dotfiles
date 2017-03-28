a8199366b5092d2c433b12bf3a3ceef8d12f05f0
Danny Thomas <dannyt@netflix.com>
2017-03-27 17:14:13 -0700

    Fix unrenamed functions

--- jvm/functions.zsh
+++ jvm/functions.zsh
@@ -1,31 +1,31 @@
 function gradleOptsUnset() {
     unset GRADLE_OPTS
-    gradle_enable_daemon
+    gradleEnableDaemon
 }
 
 function gradleOptsDebug() {
     export GRADLE_OPTS="-Xrunjdwp:server=y,transport=dt_socket,address=5005,suspend=y"
-    gradle_disable_daemon
+    gradleDisableDaemon
 }
 
 function gradleOptsYk() {
     export GRADLE_OPTS="-agentpath:/Applications/YourKit.app/Contents/Resources/bin/mac/libyjpagent.jnilib"
-    gradle_disable_daemon
+    gradleDisableDaemon
 }
 
 function gradleOptsYkSample() {
     export GRADLE_OPTS="-agentpath:/Applications/YourKit.app/Contents/Resources/bin/mac/libyjpagent.jnilib=sampling,probe_disable=*,onexit=snapshot"
-    gradle_disable_daemon
+    gradleDisableDaemon
 }
 
 function gradleOptsYkAlloc() {
     export GRADLE_OPTS="-agentpath:/Applications/YourKit.app/Contents/Resources/bin/mac/libyjpagent.jnilib=alloceach=10,allocsizelimit=4096,onexit=snapshot"
-    gradle_disable_daemon
+    gradleDisableDaemon
 }
 
 function gradleOptsYkAllocCount() {
     export GRADLE_OPTS="-agentpath:/Applications/YourKit.app/Contents/Resources/bin/mac/libyjpagent.jnilib=alloc_object_counting,onexit=snapshot"
-    gradle_disable_daemon
+    gradleDisableDaemon
 }
 
 function gradleDisableDaemon() {

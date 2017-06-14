a2db9f9ef716e6db25c6a569ef517b39bbd38095
Danny Thomas <dannyt@netflix.com>
2017-06-14 10:46:28 -0700

    Improve Gradle functions so they invoke gw directly, without affecting the global environment or gradle.properties

--- jvm/functions.zsh
+++ jvm/functions.zsh
@@ -1,45 +1,23 @@
-function gradleOptsUnset() {
-    unset GRADLE_OPTS
-    gradleEnableDaemon
+function gradleDebug() {
+    GRADLE_OPTS="-Xrunjdwp:server=y,transport=dt_socket,address=5005,suspend=y" gw --no-daemon "$@"
 }
 
-function gradleOptsDebug() {
-    export GRADLE_OPTS="-Xrunjdwp:server=y,transport=dt_socket,address=5005,suspend=y"
-    gradleDisableDaemon
-}
-
-function gradleOptsYk() {
-    export GRADLE_OPTS="-agentpath:/Applications/YourKit.app/Contents/Resources/bin/mac/libyjpagent.jnilib"
-    gradleDisableDaemon
+function gradleYk() {
+    GRADLE_OPTS="-agentpath:/Applications/YourKit.app/Contents/Resources/bin/mac/libyjpagent.jnilib" gw --no-daemon "$@"
 }
 
 function gradleOptsYkSample() {
-    export GRADLE_OPTS="-agentpath:/Applications/YourKit.app/Contents/Resources/bin/mac/libyjpagent.jnilib=sampling,probe_disable=*,onexit=snapshot"
-    gradleDisableDaemon
+    GRADLE_OPTS="-agentpath:/Applications/YourKit.app/Contents/Resources/bin/mac/libyjpagent.jnilib=sampling,probe_disable=*,onexit=snapshot" gw --no-daemon "$@"
 }
 
 function gradleOptsYkAlloc() {
-    export GRADLE_OPTS="-agentpath:/Applications/YourKit.app/Contents/Resources/bin/mac/libyjpagent.jnilib=alloceach=10,allocsizelimit=4096,onexit=snapshot"
-    gradleDisableDaemon
+    GRADLE_OPTS="-agentpath:/Applications/YourKit.app/Contents/Resources/bin/mac/libyjpagent.jnilib=alloceach=10,allocsizelimit=4096,onexit=snapshot" gw --no-daemon "$@"
 }
 
 function gradleOptsYkAllocCount() {
-    export GRADLE_OPTS="-agentpath:/Applications/YourKit.app/Contents/Resources/bin/mac/libyjpagent.jnilib=alloc_object_counting,onexit=snapshot"
-    gradleDisableDaemon
-}
-
-function gradleDisableDaemon() {
-    gsed -i 's/org.gradle.daemon=true/org.gradle.daemon=false/g' ~/.gradle/gradle.properties
-}
-
-function gradleEnableDaemon() {
-    gsed -i 's/org.gradle.daemon=false/org.gradle.daemon=true/g' ~/.gradle/gradle.properties
+    GRADLE_OPTS="-agentpath:/Applications/YourKit.app/Contents/Resources/bin/mac/libyjpagent.jnilib=alloc_object_counting,onexit=snapshot" gw --no-daemon "$@"
 }
 
 function gradleUserHomeLocal() {
-    export GRADLE_USER_HOME="$(pwd)/.gradle"
-}
-
-function gradleUserHomeUnset() {
-    unset GRADLE_USER_HOME
+    GRADLE_USER_HOME="$(pwd)/.gradle" gw "$@"
 }

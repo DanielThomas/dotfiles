4408b2823840c4e54925c7d7a54f1693524e8993
Danny Thomas <dannyt@netflix.com>
2017-07-07 08:48:45 -0700

    Add back Gradle profiling functions - they're useful for profiling the launcher

--- jvm/functions.zsh
+++ jvm/functions.zsh
@@ -11,3 +11,21 @@ function gradle-profiler() {
 	echo ""
 	~/.gradle-profiler/build/install/gradle-profiler/bin/gradle-profiler "$@"
 }
+
+# Useful for profiling the Gradle launcher
+
+function gradleYk() {
+    GRADLE_OPTS="-agentpath:/Applications/YourKit.app/Contents/Resources/bin/mac/libyjpagent.jnilib" gw --no-daemon "$@"
+}
+
+function gradleOptsYkSample() {
+    GRADLE_OPTS="-agentpath:/Applications/YourKit.app/Contents/Resources/bin/mac/libyjpagent.jnilib=sampling,probe_disable=*,onexit=snapshot" gw --no-daemon "$@"
+}
+
+function gradleOptsYkAlloc() {
+    GRADLE_OPTS="-agentpath:/Applications/YourKit.app/Contents/Resources/bin/mac/libyjpagent.jnilib=alloceach=10,allocsizelimit=4096,onexit=snapshot" gw --no-daemon "$@"
+}
+
+function gradleOptsYkAllocCount() {
+    GRADLE_OPTS="-agentpath:/Applications/YourKit.app/Contents/Resources/bin/mac/libyjpagent.jnilib=alloc_object_counting,onexit=snapshot" gw --no-daemon "$@"
+}

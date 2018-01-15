162c7bb0036b07ba9e2c8d6f048b036a0c3fa87f
Danny Thomas <dannyt@netflix.com>
2018-01-15 11:35:59 -0800

    Remove brew installer

--- brew/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-if ! test $(which brew); then
-  echo "  Installing Homebrew..."
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" > /tmp/homebrew-install.log
-  brew tap caskroom/versions
-  brew tap homebrew/versions
-fi

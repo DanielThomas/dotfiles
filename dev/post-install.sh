a3f6efa8459053d2ec1523b8587f4d7aa7d93f78
Danny Thomas <dannyt@netflix.com>
2018-08-17 10:05:02 -0700

    Ignore symlinks in Java directories

--- dev/post-install.sh
+++ dev/post-install.sh
@@ -1,3 +1,3 @@
-for java in /Library/Java/JavaVirtualMachines/*; do yes | (jenv add "$java/Contents/Home" &) > /dev/null; done
+for java in $(find /Library/Java/JavaVirtualMachines -mindepth 1 -maxdepth 1 ! -type l); do yes | (jenv add "$java/Contents/Home" & > /dev/null); done
 wait
 (jenv rehash &) 2> /dev/null

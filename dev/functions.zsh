c1c468e44211618012daa3efbdafdec721c9d976
Danny Thomas <dannyt@netflix.com>
2020-06-22 13:33:46 -0700

    Update profiler functions for latest YourKit

--- dev/functions.zsh
+++ dev/functions.zsh
@@ -15,17 +15,17 @@ function gradle-profiler() {
 # Useful for profiling the Gradle launcher and adhoc profiling
 
 function gradleYk() {
-    GRADLE_OPTS="-agentpath:/Applications/YourKit.app/Contents/Resources/bin/mac/libyjpagent.jnilib" gw --no-daemon "$@"
+    GRADLE_OPTS="-agentpath:/Applications/YourKit.app/Contents/Resources/bin/mac/libyjpagent.dylib" gw --no-daemon "$@"
 }
 
 function gradleYkSample() {
-    GRADLE_OPTS="-agentpath:/Applications/YourKit.app/Contents/Resources/bin/mac/libyjpagent.jnilib=sampling,probe_disable=*,onexit=snapshot" gw --no-daemon "$@"
+    GRADLE_OPTS="-agentpath:/Applications/YourKit.app/Contents/Resources/bin/mac/libyjpagent.dylib=sampling,probe_disable=*,onexit=snapshot" gw --no-daemon "$@"
 }
 
 function gradleYkAlloc() {
-    GRADLE_OPTS="-agentpath:/Applications/YourKit.app/Contents/Resources/bin/mac/libyjpagent.jnilib=alloceach=10,allocsizelimit=4096,onexit=snapshot" gw --no-daemon "$@"
+    GRADLE_OPTS="-agentpath:/Applications/YourKit.app/Contents/Resources/bin/mac/libyjpagent.dylib=alloceach=10,allocsizelimit=4096,onexit=snapshot" gw --no-daemon "$@"
 }
 
 function gradleYkAllocCount() {
-    GRADLE_OPTS="-agentpath:/Applications/YourKit.app/Contents/Resources/bin/mac/libyjpagent.jnilib=alloc_object_counting,onexit=snapshot" gw --no-daemon "$@"
+    GRADLE_OPTS="-agentpath:/Applications/YourKit.app/Contents/Resources/bin/mac/libyjpagent.dylib=alloc_object_counting,onexit=snapshot" gw --no-daemon "$@"
 }

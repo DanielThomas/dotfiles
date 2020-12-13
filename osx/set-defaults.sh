767a734349fbc6a3d188eb43ce2c562938b4322e
Danny Thomas <dannyt@netflix.com>
2020-12-14 08:41:20 +1100

    Update osx defaults

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -1,13 +1,18 @@
 ## Sensible defaults for Mac OS
 
-# Disable press-and-hold for keys in favor of key repeat
-defaults write -g ApplePressAndHoldEnabled -bool false
-
 # Show the ~/Library folder
 chflags nohidden ~/Library
 
-# Set a really fast key repeat
-defaults write NSGlobalDomain KeyRepeat -int 1
+# Set a really fast initial and subsequent key repeat
+defaults write -g KeyRepeat -int 1
+defaults write -g InitialKeyRepeat -int 10
+
+# Disable press-and-hold for keys in favor of key repeat
+defaults write -g ApplePressAndHoldEnabled -bool false
+
+# Disable smart quotes
+defaults write -g NSAutomaticQuoteSubstitutionEnabled -bool false
+defaults write -g NSAutomaticDashSubstitutionEnabled -bool false
 
 # Showing and hiding sheets, resizing preference windows, zooming windows
 # float 0 doesn't work

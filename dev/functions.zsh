06d57d8d983aa376a963f0bc0d9d0584052957d2
Danny Thomas <dannyt@netflix.com>
2021-03-24 10:17:30 +1100

    Remove docker_setup_vpn - RC fixes this

--- dev/functions.zsh
+++ dev/functions.zsh
@@ -29,17 +29,3 @@ function gradleYkAlloc() {
 function gradleYkAllocCount() {
     GRADLE_OPTS="-agentpath:/Applications/YourKit.app/Contents/Resources/bin/mac/libyjpagent.dylib=alloc_object_counting,onexit=snapshot" gw --no-daemon "$@"
 }
-
-function docker_setup_vpn () {
-        echo "## Previous Configuration"
-        sudo pfctl -a com.apple.internet-sharing/shared_v4 -s nat 2> /dev/null | tee /tmp/docker_vpn.conf
-        if sudo pfctl -a com.apple.internet-sharing/shared_v4 -s nat 2> /dev/null | grep 192.168.64.0 | grep -q utun2
-        then
-                echo && echo "Not applying change"
-        else
-                echo "nat on utun2 inet from 192.168.64.0/24 to any -> (utun2) extfilter ei" >> /tmp/docker_vpn.conf
-                sudo pfctl -a com.apple.internet-sharing/shared_v4 -N -f /tmp/docker_vpn.conf 2> /dev/null
-                echo && echo "## New Configuration"
-                cat /tmp/docker_vpn.conf
-        fi
-}

9d5825341501d603e7c46d09063ac158fa8393a8
Danny Thomas <dannyt@netflix.com>
2021-06-08 16:08:48 +1000

    Request specific Python version. Fix gdircolors

--- terminal/dircolors.zsh
+++ terminal/dircolors.zsh
@@ -1,5 +1,5 @@
 # Colors/GNU ls
-eval `/usr/local/bin/gdircolors $HOME/.dircolors-solarized/dircolors.256dark`
+eval `gdircolors $HOME/.dircolors-solarized/dircolors.256dark`
 
 # Grep highlight color
 export GREP_COLOR='1;31'

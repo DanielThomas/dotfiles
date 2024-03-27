82106d591dade1574fe090f21d9c7b11189a188f
Danny Thomas <dannyt@netflix.com>
2024-03-27 15:14:00 +1100

    Switch jenv to gitrepo

--- dev/path.zsh
+++ dev/path.zsh
@@ -1,4 +1,4 @@
 export GROOVY_HOME="$HOMEBREW_PREFIX/opt/groovy/libexec"
-export GOPATH=~/Projects/go
-export PATH=$GROOVY_HOME/bin:$GOPATH/bin:$PATH
-
+export GOPATH="$HOME/Projects/go"
+JENV_HOME="$HOME/.jenv"
+export PATH=$JENV_HOME/bin:$GROOVY_HOME/bin:$GOPATH/bin:$PATH

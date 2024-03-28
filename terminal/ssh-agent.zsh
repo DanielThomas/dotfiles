a58b7b40909895a9d7aeb5f95aadcb61ec9e719e
Danny Thomas <dannyt@netflix.com>
2024-03-28 12:22:08 +1100

    Remove ssh-agent for now

--- terminal/ssh-agent.zsh
+++
@@ -1,9 +0,0 @@
-if [[ "Linux" == "$(uname)" ]]; then
-	if [[ -z "$SSH_AGENT_PID" ]]; then
-		if ! ps -p $SSH_AGENT_PID > /dev/null; then
-			eval "$(ssh-agent -s)" > /dev/null
-		fi
-	else
-		eval "$(ssh-agent -s)" > /dev/null
-	fi
-fi

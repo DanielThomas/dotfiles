eddc35555b217047db073f6d89a00453961ef18c
Danny Thomas <dannyt@netflix.com>
2024-03-28 11:43:47 +1100

    Make ps conditional

--- terminal/ssh-agent.zsh
+++ terminal/ssh-agent.zsh
@@ -1,5 +1,9 @@
 if [[ "Linux" == "$(uname)" ]]; then
-	if ! ps -p $SSH_AGENT_PID > /dev/null; then
+	if [[ -z "$SSH_AGENT_PID" ]]; then
+		if ! ps -p $SSH_AGENT_PID > /dev/null; then
+			eval "$(ssh-agent -s)" > /dev/null
+		fi
+	else
 		eval "$(ssh-agent -s)" > /dev/null
 	fi
 fi

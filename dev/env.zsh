1abd9452730f54697acb0c397c913dadce6605c0
Danny Thomas <dannyt@netflix.com>
2024-03-28 13:23:04 +1100

    Fix jenv

--- dev/env.zsh
+++ dev/env.zsh
@@ -1,4 +1,4 @@
 export ANT_HOME=$HOMEBREW_PREFIX/opt/ant/libexec/
 export GROOVY_HOME="$HOMEBREW_PREFIX/opt/groovy/libexec"
 export GOPATH="$HOME/Projects/go"
-export JENV_HOME="$HOME/.jenv"
+
--- dev/jenv.zsh
+++ dev/jenv.zsh
@@ -1,3 +1,6 @@
+export JENV_HOME="$HOME/.jenv"
+export PATH=$JENV_HOME/bin:$PATH
+
 eval "$(jenv init - --no-rehash)"
 (jenv rehash &) 2> /dev/null
 jenv enable-plugin export >  /dev/null

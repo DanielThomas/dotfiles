37dc617076737c4a727ae291bd4da2ac0834b010
Danny Thomas <dannyt@netflix.com>
2024-03-28 13:11:40 +1100

    Cleanup paths. Add .local/bin

--- dev/env.zsh
+++ dev/env.zsh
@@ -1,2 +1,4 @@
-export PATH=$PATH:$HOME/.cargo/bin:$HOMEBREW_PREFIX/opt/python@3.9/libexec/bin:$HOME/.gdub/bin
 export ANT_HOME=$HOMEBREW_PREFIX/opt/ant/libexec/
+export GROOVY_HOME="$HOMEBREW_PREFIX/opt/groovy/libexec"
+export GOPATH="$HOME/Projects/go"
+export JENV_HOME="$HOME/.jenv"
--- dev/path.zsh
+++ dev/path.zsh
@@ -1,4 +1,1 @@
-export GROOVY_HOME="$HOMEBREW_PREFIX/opt/groovy/libexec"
-export GOPATH="$HOME/Projects/go"
-JENV_HOME="$HOME/.jenv"
-export PATH=$JENV_HOME/bin:$GROOVY_HOME/bin:$GOPATH/bin:$PATH
+export PATH=$HOME/.local/bin:$HOME/.gdub/bin:$JENV_HOME/bin:$GROOVY_HOME/bin:$GOPATH/bin:$PATH

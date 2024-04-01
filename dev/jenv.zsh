51aaf663d56e96b0c073ec6e918fcdc64cd8e08a
Danny Thomas <dannyt@netflix.com>
2024-04-01 16:15:21 +1100

    Turn off jenv on workspace, it's provided

--- dev/jenv.zsh
+++ dev/jenv.zsh
@@ -1,6 +1,8 @@
-export JENV_HOME="$HOME/.jenv"
-export PATH=$JENV_HOME/bin:$PATH
+if [ ! -z "$CODER_WORKSPACE_NAME" ]; then
+	export JENV_HOME="$HOME/.jenv"
+	export PATH=$JENV_HOME/bin:$PATH
 
-eval "$(jenv init - --no-rehash)"
-(jenv rehash &) 2> /dev/null
-jenv enable-plugin export >  /dev/null
+	eval "$(jenv init - --no-rehash)"
+	(jenv rehash &) 2> /dev/null
+	jenv enable-plugin export >  /dev/null
+fi

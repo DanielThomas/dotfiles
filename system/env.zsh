fb98ff47e6696c790f9b68a38c4fa7bef4f63384
Danny Thomas <dannyt@netflix.com>
2024-04-19 08:00:35 +1000

    Remove opener

--- system/env.zsh
+++ system/env.zsh
@@ -1,6 +1,2 @@
 export EDITOR="st -w"
 export HOMEBREW_NO_ENV_HINTS=1
-
-if [[ "Darwin" != "$(uname)" ]]; then
-	export BROWSER=opener-browser
-fi
--- system/post-install.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-if [[ "Darwin" == "$(uname)" ]]; then
-	brew services start opener
-fi

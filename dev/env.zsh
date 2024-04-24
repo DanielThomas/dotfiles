1ed69c576fbcaf025efc7e13dc9a7e1757e8d77a
Danny Thomas <dannyt@netflix.com>
2024-04-24 11:43:05 +1000

    Remove unnecessary environment variables

--- dev/env.zsh
+++
@@ -1,4 +0,0 @@
-export ANT_HOME=$HOMEBREW_PREFIX/opt/ant/libexec/
-export GROOVY_HOME="$HOMEBREW_PREFIX/opt/groovy/libexec"
-export GOPATH="$HOME/Projects/go"
-
--- dev/path.zsh
+++
@@ -1,1 +0,0 @@
-export PATH=$HOME/.local/bin:$GROOVY_HOME/bin:$GOPATH/bin:$PATH

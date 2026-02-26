e6b3723725c6539da73f1f47c15c091e901c7c90
Danny Thomas <dannyt@netflix.com>
2026-02-26 11:00:49 +1100

    Temporary fix for ghclone while I work our defaults for gh

--- git/ghclone.zsh
+++ git/ghclone.zsh
@@ -8,7 +8,7 @@ ghclone() {
         repodir="$GITHUB_DIR/$1"
         if [ ! -d "$repodir" ]; then
             if [[ "$1" == corp/* ]]; then
-                gh repo clone "$1" "$repodir"
+                GH_HOST="$GHE_HOST" gh repo clone "$1" "$repodir"
             else
                 GH_HOST="github.com" gh repo clone "$1" "$repodir"
             fi
